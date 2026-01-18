cd5917af5590f62cf4ff3dc52ac303f55bd84495
mohammadRaiyyan <inforaiyyan@gmail.com>
2026-01-18 14:29:07 +0530

    refactor: added support to execute generic function

--- javascript/currying.js
+++ javascript/currying.js
@@ -27,7 +27,41 @@ function generateSum(executeLimit) {
   };
 }
 
-const sum = generateSum(4);
-console.log(sum(1)(2)(3)(4)); // 10
-console.log(sum(1, 2, 3, 4, 5, 6)); // 10
-console.log(sum(1)(2, 3, 4)); // 10
+// const sum = generateSum(4);
+// console.log(sum(1)(2)(3)(4)); // 10
+// console.log(sum(1, 2, 3, 4, 5, 6)); // 10
+// console.log(sum(1)(2, 3, 4)); // 10
+
+/*
+Example v2:
+    const sum = generateSum(4);
+    console.log(sum(1)(2)(3)(4))// 10
+    console.log(sum(1,2,3,4,5,6)) // 10
+    console.log(sum(1)(2,3,4)) // 10
+*/
+
+function curry(fn) {
+  if (!fn) {
+    throw new Error(
+      `Curry function expects a function to perform the operation but it got ${typeof fn} `,
+    );
+  }
+  return function executer(...args) {
+    if (args.length < fn.length) {
+      return (...nextArgs) => {
+        return executer(...args, ...nextArgs);
+      };
+    } else {
+      return fn.apply(this, args);
+    }
+  };
+}
+
+function sum(a, b, c, d) {
+  return a + b + c + d;
+}
+
+const calculate = curry(sum);
+console.log(calculate(1)(2)(3)(4)); // 10
+console.log(calculate(1, 2, 3, 4, 5, 6)); // 10
+console.log(calculate(1)(2, 3, 4)); // 10
